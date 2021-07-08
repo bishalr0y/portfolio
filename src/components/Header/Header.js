18a9e31574874c090da342e5553340a968284a08
bishalr0y <bishalroy895@gmail.com>
2021-07-08 11:00:48 +0530

    create navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,42 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillCode } from 'react-icons/ai';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display: "flex", aligiItems: "center", color: "white", marginBottom: "20px"}}>
+          <AiFillCode /> <Span> Bishal</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li href="#projects">
+        <NavLink>Projects</NavLink>
+      </li>
+      <li href="#tech">
+        <NavLink>Technologies</NavLink>
+      </li>
+      <li href="#about">
+        <NavLink>About</NavLink>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/bishalr0y">
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com/in/bishalr0y">
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://twitter.com/bishalr0y">
+        <AiFillTwitterCircle size="3rem"/>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
